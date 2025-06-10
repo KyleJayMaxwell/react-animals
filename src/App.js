1900fa609d1ba5ce3ebae0cc1a3d29ee7bc2f9d3
Kyle Maxwell <kylejaymaxwell@gmail.com>
2025-06-09 23:42:59 -0700

    event system

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 function App () {
-    return <div>Show animal list here!</div>
+    const handleClick = () => {
+        console.log('Button was clicked');
+    };
+
+    return <div>
+        <button onClick={handleClick}>Add Animal</button>
+    </div>
 }
 export default App;
